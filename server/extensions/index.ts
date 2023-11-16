cf27af96d9125834dd1d53cd20e35414cd3443f2
Alias <Error122122122122122@yandex.ru>
2023-11-16 21:05:50 +0800

    changes

--- server/extensions/index.ts
+++ server/extensions/index.ts
@@ -1,7 +1,7 @@
 import fp from "fastify-plugin"
 import { FastifyInstance } from "fastify"
-import fastifyCors from "@fastify/cors"
 export const addAllPlugins = fp(function(instance:FastifyInstance,opts:Object,done:Function){
-    instance.register(fastifyCors)
+    instance.register(require("@fastify/cors"))
+    instance.register(require("@fastify/formbody"))
     done()
 })
